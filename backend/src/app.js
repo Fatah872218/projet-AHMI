4900027fa6e3b2af8abd44fa7fff5670046c7e93
Fatah872218 <fatah_djeddi@yahoo.fr>
2025-04-08 12:14:48 +0200

    Résolution des conflits après pull

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,44 +3,22 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./config/db.js";
-<<<<<<< Updated upstream
-import authRoutes from "./routes/auth.routes.js"; //
-=======
-import utilisateurRoutes from "./routes/routeUtilisateur.js";
->>>>>>> Stashed changes
 
-console.log("MONGO_URI =", process.env.MONGO_URI);
+import utilisateurRoutes from "./routes/routeUtilisateur.js";
 
 const app = express();
-<<<<<<< Updated upstream
-// Connexion à la BDD
-=======
-
-// Middleware
-app.use(cors()); // Autoriser les requêtes cross-origin
-app.use(express.json()); // Parser les requêtes JSON
-app.use(cookieParser()); // Utiliser cookie-parser
 
 // Connexion à la base de données
->>>>>>> Stashed changes
 connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json()); //
 app.use(cookieParser());
 
-<<<<<<< Updated upstream
-// Déclaration des routes
-app.use("/api/auth", authRoutes); //
-
-// Lancement du serveur
-const PORT = process.env.PORT || 5000;
-=======
 // Routes
 app.use("/api/utilisateurs", utilisateurRoutes);
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
->>>>>>> Stashed changes
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur http://localhost:${PORT}`);
 });
