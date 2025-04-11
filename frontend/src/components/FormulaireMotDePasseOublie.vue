05a8e0e36b633c845a89c9cc63ac7416b50ab4d6
Elise <eliseg64@gmail.com>
2025-04-11 09:42:52 +0200

    ajout de fichiers utiles pour la gestion utilisateurs

---
+++ frontend/src/components/FormulaireMotDePasseOublie.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- Composant pour le Un formulaire simple avec un champ email  -->
+    <p>essai !</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Bonjour, monde !',
+    }
+  },
+  methods: {
+    // Composant pour Un formulaire simple avec un champ email .
+    greet() {
+      console.log(this.message)
+    },
+  },
+}
+
+/*
+Composant pour Un formulaire simple avec un champ email 
+*/
+</script>
+
+<style scoped>
+/* Composant pour Un formulaire simple avec un champ email  */
+p {
+  color: blue;
+}
+</style>
---
+++ frontend/src/views/VueReinitialisationMotDePasse.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!--  Vue utilisée avec un token dans l'URL (ex: /reset-password/:token) Contient un formulaire. -->
+    <p>vue avec formulaire : nouveau mot de passe, confirmation et bouton Réinitialiser</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Bonjour, monde !',
+    }
+  },
+  methods: {
+    //  Vue utilisée avec un token dans l'URL (ex: /reset-password/:token) Contient un formulaire.
+    greet() {
+      console.log(this.message)
+    },
+  },
+}
+
+/*
+ Vue utilisée avec un token dans l'URL (ex: /reset-password/:token) Contient un formulaire.
+*/
+</script>
+
+<style scoped>
+/*  Vue utilisée avec un token dans l'URL (ex: /reset-password/:token) Contient un formulaire */
+p {
+  color: blue;
+}
+</style>
