be39ae18c490fe1119b5d715aa545fa6e227e290
Elise <eliseg64@gmail.com>
2025-04-17 09:47:13 +0200

    Récupération des modifications du dossier backend de la branche dev

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,17 +10,10 @@ import authRoutes from "./routes/routesAuth.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
 // Connexion à la base de données
 connectDB();
 // Middleware
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json()); //
 app.use(cookieParser());
 
